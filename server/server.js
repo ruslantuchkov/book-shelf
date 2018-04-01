45d9ef77b0ede55853dc42e2ce61cbefe8738f22
ruslantuchkov <nidjel87@gmail.com>
2018-04-01 13:04:33 +0500

    added models

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/config').get(process.env.NODE_ENV);
-
 const app = express();
-app.use(require('body-parser').json());
-app.use(require('cookie-parser')());
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE);
+
+const { User } = require('./models/user');
+const { Book } = require('./models/book');
+
+app.use(require('body-parser').json());
+app.use(require('cookie-parser')());
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`server is running on port: ${port}!`));
