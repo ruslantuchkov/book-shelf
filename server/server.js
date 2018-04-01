7aa561a09183a1398cc8f478e9e0aa480b70f31e
ruslantuchkov <nidjel87@gmail.com>
2018-04-01 18:32:33 +0500

    user routes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ mongoose.connect(config.DATABASE);
 
 const { User } = require('./models/user');
 const { Book } = require('./models/book');
+const auth = require('./middleware/auth');
 
 app.use(require('body-parser').json());
 app.use(require('cookie-parser')());
@@ -38,6 +39,49 @@ app.get('/api/books', (req, res) => {
     });
 });
 
+app.get('/api/getReviewer', (req, res) => {
+  let id = req.query.id;
+
+  User.findById(id, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.json({
+      name: doc.name,
+      lastname: doc.lastname
+    });
+  });
+});
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json(users);
+  });
+});
+
+app.get('/api/user_posts', (req, res) => {
+  Book.find({ ownerId: req.query.user }).exec((err, docs) => {
+    if (err) return res.status(400).send(err);
+    res.send(docs);
+  });
+});
+
+app.get('/api/logout', auth, (req, res) => {
+  req.user.deleteToken(req.token, (err, user) => {
+    if (err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+});
+
+app.get('/api/auth', auth, (req, res) => {
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname
+  });
+});
+
 // POST //
 
 app.post('/api/book', (req, res) => {
@@ -47,6 +91,39 @@ app.post('/api/book', (req, res) => {
     res.status(200).json({
       post: true,
       bookId: doc._id
+    });
+  });
+});
+
+app.post('/api/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false });
+    res.status(200).json({ success: true, user: doc });
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        isAuth: false,
+        message: 'Auth failed, email not found.'
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ isAuth: false, message: 'Wrong password' });
+      }
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie('auth', user.token).json({
+          isAuth: true,
+          id: user._id,
+          email: user.email
+        });
+      });
     });
   });
 });
@@ -67,7 +144,7 @@ app.delete('/api/delete_book', (req, res) => {
 
   Book.findByIdAndRemove(id, (err, doc) => {
     if (err) return res.status(400).send(err);
-    res.json(true);
+    res.json({ success: true });
   });
 });
 
